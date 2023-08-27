d0e622d322f76601ff4c50629e21bebf94b3763e
SongtingY <sy642@scarletmail.rutgers.edu>
2023-08-26 19:49:46 -0700

    关注

--- community/src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ community/src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -7,11 +7,29 @@ public class RedisKeyUtil {
 
     private static final String PREFIX_USER_LIKE = "like:user";
 
+    private static final String PREFIX_FOLLOWEE = "like:followee";
+
+    private static final String PREFIX_FOLLOWER = "like:follower";
+
+//    某个实体的赞
     public static String getEntityLikeKey(int entityType, int entityId){
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
     }
 
+//    某个用户的赞
     public static String getUserLikeKey(int userId){
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+//    某个用户关注的实体
+//    followee:userId:entityType -> zset(entityId, now) now:关注的时间顺序（业务需求）
+    public static String getFolloweeKey(int userId, int entityType){
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+//    某个实体拥有的粉丝
+//    follower:entityType:entityId
+    public static String getFollowerKey(int entityType, int entityId){
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 }
