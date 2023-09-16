856efc5edd6bba7d3306d8297c73fc020b9e1fc8
SongtingY <sy642@scarletmail.rutgers.edu>
2023-09-15 23:33:14 -0700

    完成caffeine cache 通过了压力测试

--- community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ community/src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -1,10 +1,14 @@
 package com.nowcoder.community.service;
 
+import com.github.benmanes.caffeine.cache.CacheLoader;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.util.SensitiveFilter;
 import jakarta.annotation.PostConstruct;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class DiscussPostService {
@@ -41,14 +46,60 @@ public class DiscussPostService {
     // 帖子总数的缓存
     private LoadingCache<Integer, Integer> postRowsCache;
 
-//    @PostConstruct
-//    public
+    @PostConstruct
+    public void init(){
+        // 初始化帖子列表缓存
+        postListCache = Caffeine.newBuilder()
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<String, List<DiscussPost>>() {
+                    @Override
+                    public @Nullable List<DiscussPost> load(String key) throws Exception {
+                        if (key == null || key.length() == 0) {
+                            throw new IllegalArgumentException("参数错误！");
+                        }
+                        String[] params = key.split(":");
+                        if (params == null || params.length != 2) {
+                            throw new IllegalArgumentException("参数错误！");
+                        }
+                        int offset = Integer.valueOf(params[0]);
+                        int limit = Integer.valueOf(params[1]);
+
+                        // 这里可以加二级缓存：Redis -> mysql
+                        // 等后续再增加二级Redis缓存，暂时先不实现。
+
+                        logger.debug("load post list from DB.");
+                        return discussPostMapper.selectDiscussPosts(0, offset, limit, 1);
+                    }
+                });
+//        初始化帖子总数缓存
+                postRowsCache = Caffeine.newBuilder()
+                //这里本来应该单独在application.properties里配置的，如：caffeine.posts-count.max-size和caffeine.posts-count.expire-seconds。但老师懒的单独配，复用缓存帖子的也不出错，所以这样了。
+                .maximumSize(maxSize)
+                .expireAfterWrite(expireSeconds, TimeUnit.SECONDS)
+                .build(new CacheLoader<Integer, Integer>() {
+                    @Nullable
+                    @Override
+                    public Integer load(@NonNull Integer key) throws Exception {
+                        logger.debug("load post rows from DB.");
+                        return discussPostMapper.selectDiscussPostRows(key);
+                    }
+                });
+    }
 
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit, int orderMode){
+        if(userId == 0 && orderMode == 1){
+            return postListCache.get(offset + ":" + limit);
+        }
+        logger.debug("load post list from DB.");
         return discussPostMapper.selectDiscussPosts(userId, offset, limit, orderMode);
     }
 
     public int findDiscussPostRows(int userId){
+        if(userId == 0){
+            return postRowsCache.get(userId);
+        }
+        logger.debug("load post list from DB.");
         return discussPostMapper.selectDiscussPostRows(userId);
     }
 
